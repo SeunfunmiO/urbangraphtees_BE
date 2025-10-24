863a535c2bf2c5b975cc1ca4b4283f40b8b126ab
seunfunmi <seunfunmisore89@gmail.com>
2025-10-24 21:52:03 +0100

    `Update CORS configuration and file upload limits in index.js`

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173/",
+        "https://urbangraphtees-fe.vercel.app/"
+    ],
+    credentials: true
+}));
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -15,8 +21,8 @@ const wishlistRoute = require('./routes/wishlist.routes.js')
 const cartRoute = require('./routes/cart.routes.js')
 const notificationRoute = require('./routes/notification.routes.js')
 const path = require('path')
-app.use(express.urlencoded({limit:'5mb' ,extended: true }))
-app.use(express.json({limit:'5mb'}))
+app.use(express.urlencoded({ limit: '5mb', extended: true }))
+app.use(express.json({ limit: '5mb' }))
 app.use('/user', userRoute)
 app.use('/newsletter', newsletterRoute)
 app.use('/products', productRoute)
